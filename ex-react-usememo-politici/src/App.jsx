ac8e8d21fd8da7b03bde40d8918ced09781136dd
osaze <osazezaboli@gmail.com>
2026-01-03 03:41:25 +0100

    refactor filtri e codice

--- ex-react-usememo-politici/src/App.jsx
+++ ex-react-usememo-politici/src/App.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import Card from "./components/card";
 import "./App.css";
 function App() {
+  // fetch lista di politici
   const [politicians, setPoliticians] = useState([]);
   useEffect(() => {
     fetch("http://localhost:3333/politicians")
@@ -9,22 +10,29 @@ function App() {
       .then((res) => setPoliticians(res))
       .catch((err) => console.error(err));
   }, []);
-  const [filteredList, setFilteredList] = useState();
-  const [inputTxt, setInputTxt] = useState("");
-  function handleChangeFilter(e) {
-    const inputTerm = e.target.value;
-    setInputTxt(inputTerm);
-    const filteredP = politicians.filter(
-      (p) =>
-        p.name.toLowerCase().includes(inputTerm.toLowerCase()) ||
-        p.biography.toLowerCase().includes(inputTerm.toLowerCase())
-    );
-    if (!inputTerm) {
-      setFilteredList(politicians);
-    } else {
-      setFilteredList(filteredP);
+
+  // creazione di array di tutte le posizioni presenti
+  const PolPositions = politicians?.reduce((acc, curr) => {
+    if (!acc.includes(curr.position)) {
+      acc.push(curr.position);
     }
-  }
+    return acc;
+  }, []);
+
+  // filtro politico
+  const [inputTxt, setInputTxt] = useState("");
+  const [selected, setSelected] = useState("Default");
+
+  const filteredList = useMemo(() => {
+    return politicians.filter((p) => {
+      const validInput = p.name.toLowerCase().includes(inputTxt.toLowerCase());
+      const validBio = p.biography
+        .toLowerCase()
+        .includes(inputTxt.toLowerCase());
+      const validSelect = selected === "Default" || selected === p.position;
+      return (validInput || validBio) && validSelect;
+    });
+  }, [politicians, inputTxt, selected]);
 
   return (
     <>
@@ -32,12 +40,22 @@ function App() {
       <input
         type="text"
         value={inputTxt}
-        onChange={(e) => handleChangeFilter(e)}
+        onChange={(e) => setInputTxt(e.target.value)}
       />
+      <select name="positions" onChange={(e) => setSelected(e.target.value)}>
+        <option value="Default">Tutte le posizioni</option>
+        {PolPositions.map((p, i) => {
+          return (
+            <option key={i} value={p}>
+              {p}
+            </option>
+          );
+        })}
+      </select>
       <div className="card-grid">
-        {filteredList
-          ? filteredList.map((p) => <Card key={p.id} politician={p} />)
-          : politicians.map((p) => <Card key={p.id} politician={p} />)}
+        {filteredList.map((p) => (
+          <Card key={p.id} politician={p} />
+        ))}
       </div>
     </>
   );
